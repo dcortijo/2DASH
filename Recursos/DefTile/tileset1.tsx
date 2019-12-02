0afa7f782f74e608461ab1f9113819c5b746b933
Daniel Cortijo Gamboa <dcortijo@ucm.es>
2019-12-02 12:20:16 +0100

    Creado el tileset del nivel 1 y empezado a crear mapa del mismo. También decidido cual es el método que mantendrá en el aire al jugador al atacar.

---
+++ Recursos/DefTile/tileset1.tsx
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="tileset1" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="TilesetFirst.png" width="512" height="512"/>
+ <terraintypes>
+  <terrain name="Edificio" tile="0"/>
+ </terraintypes>
+ <tile id="0" terrain=",,,0" probability="0.5"/>
+ <tile id="1" terrain=",,,0" probability="0.5"/>
+ <tile id="2" terrain=",,0," probability="0.5"/>
+ <tile id="8" terrain=",,0,0" probability="0.47"/>
+ <tile id="9" terrain=",,0,0" probability="0.47"/>
+ <tile id="10" terrain=",,0," probability="0.5"/>
+ <tile id="18" terrain=",,0,0" probability="0.06"/>
+ <tile id="19" terrain=",0,,0" probability="0.65"/>
+ <tile id="20" terrain="0,,0," probability="0.65"/>
+ <tile id="27" terrain=",0,,0" probability="0.117"/>
+ <tile id="28" terrain="0,,0," probability="0.117"/>
+ <tile id="32" terrain="0,0,0,0" probability="0.017"/>
+ <tile id="33" terrain="0,0,0,0" probability="0.017"/>
+ <tile id="34" terrain="0,0,0,0" probability="0.017"/>
+ <tile id="35" terrain=",0,,0" probability="0.117"/>
+ <tile id="36" terrain="0,,0," probability="0.117"/>
+ <tile id="40" terrain="0,0,0,0" probability="0.017"/>
+ <tile id="41" terrain="0,0,0,0" probability="0.017"/>
+ <tile id="42" terrain="0,0,0,0" probability="0.017"/>
+ <tile id="43" terrain=",0,,"/>
+ <tile id="44" terrain="0,,,"/>
+ <tile id="48" terrain="0,0,0,0" probability="0.85"/>
+ <tile id="49" terrain="0,0,0,0" probability="0.017"/>
+ <tile id="50" terrain="0,0,0,0" probability="0.017"/>
+ <tile id="51" terrain=",0,,0" probability="0.117"/>
+ <tile id="52" terrain="0,,0," probability="0.117"/>
+ <tile id="56" terrain="0,0,," probability="0.017"/>
+ <tile id="57" terrain="0,0,," probability="0.15"/>
+ <tile id="58" terrain="0,0,," probability="0.15"/>
+ <tile id="59" terrain="0,0,0,"/>
+ <tile id="60" terrain="0,0,,0"/>
+ <tile id="61" terrain="0,0,," probability="0.7"/>
+</tileset>
