2debb444fea06985d9e2a90382d654041c074aa5
Daniel Cortijo Gamboa <dcortijo@ucm.es>
2019-12-09 19:35:12 +0100

    Creada clase MovingPlatform.

---
+++ docs/MovingPlatform.js
@@ -0,0 +1,23 @@
+import PhysSprite from './PhysSprite.js';
+import TriggerFlyerComponent from './TriggerFlyerComponent.js';
+import TriggerStayMessage from './TriggerStayMessage.js';
+export default class MovingPlatform extends PhysSprite{
+    constructor(config){ // config + {objectives[], speed}
+        super(config);
+        this.components = [new TriggerFlyerComponent(this, config.objectives, config.speed)];
+    }
+
+    OnTriggerStay(body, other){
+        super.OnTriggerStay(body, other);
+        this.components.forEach(element => {
+            element.receive(new TriggerStayMessage(this, body, other));
+        });
+    }
+
+    preUpdate(time, delta){
+        super.preUpdate(time, delta);
+        this.components.forEach(element => {
+            element.update(time, delta);
+        });
+    }
+}
