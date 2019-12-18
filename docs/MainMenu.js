097abde5037f1a38ee87d39440d771c774227e89
Daniel Cortijo <dcortijo@ucm.es>
2019-12-18 12:05:39 +0100

    Puesto archivos que faltaban

---
+++ docs/MainMenu.js
@@ -0,0 +1,19 @@
+export default class LoadResources extends Phaser.Scene{
+    constructor(keyname) {
+        super({key: "Menu"});
+    }
+
+    preload(){
+        this.input = this.input.keyboard.addKeys('enter');
+        this.input.enter.on('down', event =>{this.scene.start('Nivel1');});
+    }
+
+    create(){
+        // Background
+      let background = this.add.image(-100, 0, 'menu');
+      background.setOrigin(0, 0);
+      background.displayWidth = 1640;
+      background.displayHeight = 810;
+      background.setScrollFactor(0);
+    }
+}
