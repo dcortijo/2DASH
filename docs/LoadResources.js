7767ca20eaa537e1259ac1c3b2a80fbc45e32aa5
Eloy Moreno <eloymore@ucm.es>
2020-01-10 22:38:31 +0100

    Actualización de mantenimiento. Arreglada lluvia
    Al parecer, a node.js le dan igual las mayúsculas a la hora de cargar archivos, porque el archivo se llama Raindrop con mayúscula. En local no se produce error, pero en Github Pages sí.

--- docs/LoadResources.js
+++ docs/LoadResources.js
@@ -36,7 +36,7 @@ export default class LoadResources extends Phaser.Scene{
         this.load.image('bullet', './img/Bullet.png');
         this.load.image('misiluro', './img/Misiluro.png');
         this.load.image('cannon', './img/MisiluroCannon.png');
-        this.load.image('raindrop', './img/raindrop.png');
+        this.load.image('raindrop', './img/Raindrop.png');
 
         //Sonidos
         this.load.audio('hurt', './audio/pain.mp3');
